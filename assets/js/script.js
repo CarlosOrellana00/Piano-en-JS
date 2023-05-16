db33ddbd0afb512170adc83eeda455f40dd06a0c
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-16 16:40:04 -0400

    key error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio("assets/audio/a.wav");//audio por defecto
+let allKey =[],
+audio = new Audio("assets/audio/a.wav");//audio por defecto
 
 const playTune = (key) => {
   audio.src = `assets/audio/${key}.wav`;
   audio.play();
+  // console.log(allKey);
+
+  const clickedKey = document.querySelector(`[data-key = "${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() =>{
+    clickedKey.classList.remove("active");
+  },150);
 }
 
 pianoKeys.forEach(key => {
+  allKey.push(key.dataset.key);
   key.addEventListener("click", () => playTune(key.dataset.key));
 });
 
 const pressedKey = (e) => {
-  playTune(e.key);
+  if(allKey.includes(e.key)) playTune(e.key);
 }
 
 document.addEventListener("keydown", pressedKey);
