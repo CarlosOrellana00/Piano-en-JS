a48a449f811c88aefd1289371c09700587809681
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-16 15:41:54 -0400

    first sound

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+let audio = new Audio("assets/audio/a.wav");//audio por defecto
+
+const playTune = (key) => {
+  audio.play();
+}
+
 pianoKeys.forEach(key => {
-  console.log(key);
+  key.addEventListener("click", () => playTune(key.CDATA_SECTION_NODE.key));
 });
