c644599372ab4604e3dbe622f918d63a013f1353
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-05-16 16:09:45 -0400

    teclado funciona, letras y por teclado

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,16 @@ const pianoKeys = document.querySelectorAll(".piano-keys .key");
 let audio = new Audio("assets/audio/a.wav");//audio por defecto
 
 const playTune = (key) => {
+  audio.src = `assets/audio/${key}.wav`;
   audio.play();
 }
 
 pianoKeys.forEach(key => {
-  key.addEventListener("click", () => playTune(key.CDATA_SECTION_NODE.key));
+  key.addEventListener("click", () => playTune(key.dataset.key));
 });
+
+const pressedKey = (e) => {
+  playTune(e.key);
+}
+
+document.addEventListener("keydown", pressedKey);
